6f0bd7587b6ff8be6ab8d678579b2ef0d8c14ecc
David Krcek <david@rotecodefraktion.de>
2022-07-22 12:25:55 +0200

    Initial Commit

---
+++ MaterialBackgroundSheetView/MaterialBackgroundSheetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialBackgroundSheetViewApp.swift
+//  MaterialBackgroundSheetView
+//
+//  Created by David Krcek on 22.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MaterialBackgroundSheetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
